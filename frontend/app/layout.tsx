5954b638e03d92655ab5bd89c05cd0d05856d2b0
Nirupan M <nirupanm007@gmail.com>
2025-04-26 22:40:28 +0530

    Fix layout

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,12 @@
+// app/layout.tsx
+import './globals.css'; // if you have a global stylesheet, optional
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
